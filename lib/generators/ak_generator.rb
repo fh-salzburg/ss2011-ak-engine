bef9881e4c6ac1c8e7d1a390d55715e9adf6770e
Michael Maier <m.maier@mmwebdesign.at>
2011-05-18 11:29:58 +0200

    adds color setter

--- lib/generators/ak_generator.rb
+++ lib/generators/ak_generator.rb
@@ -1,14 +1,15 @@
 module Rails
   class AkGenerator < Rails::Generators::Base
 		source_root File.expand_path("../templates", __FILE__)
+		class_option :color, :type => :string
 		
 		def create_stylesheets
-			template 'ak-stylesheet.css', 'public/stylesheets/ak-stylesheet.css'
+			template "ak-stylesheet.css", "public/stylesheets/ak-stylesheet.css"
 			puts "Stylesheet created!"
 		end
 		
 		def add_fontsize
-	    append_file 'public/stylesheets/ak-stylesheet.css', 'p {font-size: 12px;}'
+	    append_file "public/stylesheets/ak-stylesheet.css", "p {color: #{options.color};}"
 			puts "Fontsize defined!"
 	  end
   end
