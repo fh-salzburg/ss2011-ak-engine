ac2125c1f525836d0c3a0029caded2fb07418b6c
Bettina Steger <bettina_steger@gmx.at>
2011-05-18 11:16:02 +0200

    third step with own static layout

--- lib/generators/style_generator.rb
+++ lib/generators/style_generator.rb
@@ -2,12 +2,13 @@ module Rails
   module Generators
     class StyleGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-      argument :attributes, :type => :string, :default => "black",
-          :banner => "color as string"
+      argument :attributes, :type => :string, :default => "black", :banner => "color as string"
       
       def copy_stylesheet
         y "setting color of h1 to #{attributes}"
-        template "application.css", "public/stylesheets/application.css"
+        css_file = "application.css"
+        template css_file, "public/stylesheets/#{css_file}"
+        copy_file "application.html.erb", "app/views/layouts/static.html.erb"
       end
       
     end
