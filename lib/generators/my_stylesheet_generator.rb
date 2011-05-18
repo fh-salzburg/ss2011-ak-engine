0ab58dcaf4ceefc4763de0ff88669b90280e639d
Matthias Frick <matthias@frick-web.at>
2011-05-18 11:08:54 +0200

    added flag for own color in stylesheet

--- lib/generators/my_stylesheet_generator.rb
+++ lib/generators/my_stylesheet_generator.rb
@@ -2,6 +2,7 @@ module Rails
   module Generators
     class MyStylesheetGenerator < Rails::Generators::Base
       source_root File.expand_path("../template", __FILE__)
+      class_option :color, :type => :string, :default => '#666'
       
       def generate_layout
         copy_file "my_layout.html.erb", "app/views/layouts/application.html.erb"
@@ -9,6 +10,10 @@ module Rails
       
       def generate_stylesheet
         copy_file "my_stylesheet.css", "public/stylesheets/my_stylesheet.css"
+      end
+      
+      def generate_own_background_color
+        append_file "public/stylesheets/my_stylesheet.css", "body {color: #{options.color};}"
       end
       
     end
