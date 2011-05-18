77068a14fe8d05303d15d2083aab71650615d0a6
Bettina Steger <bettina_steger@gmx.at>
2011-05-18 10:58:10 +0200

    third step, own text color for stylesheet

--- lib/generators/style_generator.rb
+++ lib/generators/style_generator.rb
@@ -1,11 +1,13 @@
 module Rails
   module Generators
     class StyleGenerator < Rails::Generators::Base
-      
       source_root File.expand_path("../templates", __FILE__)
+      argument :attributes, :type => :string, :default => "black",
+          :banner => "color as string"
       
       def copy_stylesheet
-        copy_file "application.css", "public/stylesheets/application.css"
+        y "setting color of h1 to #{attributes}"
+        template "application.css", "public/stylesheets/application.css"
       end
       
     end
